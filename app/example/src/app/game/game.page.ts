f917d1e8922f5d80b2995be65261785ef7cfb41b
MalekMorad <malek-morad@hotmail.com>
2022-10-09 23:14:22 +0200

    implements character animations and controls

--- app/example/src/app/game/game.page.ts
+++ app/example/src/app/game/game.page.ts
@@ -9,11 +9,12 @@ class GameScene extends Phaser.Scene {
   static platform: Platform;
   static width: number;
   static height: number;
-  private background: Phaser.GameObjects.TileSprite;
+  private backgroundSky: Phaser.GameObjects.TileSprite;
   private character: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
   private cursors: Phaser.Types.Input.Keyboard.CursorKeys;
   private pointer: Phaser.Input.Pointer;
-  private currentPosY = 0;
+
+  private outerCam: Phaser.Cameras.Scene2D.Camera;
 
   private grounds: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody[] = [];
 
@@ -22,32 +23,53 @@ class GameScene extends Phaser.Scene {
   }
 
   preload() {
+    this.load.image('sky', 'assets/packs/country-platform-files/layers/country-platform-back.png');
     this.load.image('nightSky', 'assets/backgrounds/night.jpg');
     this.load.image('ground', 'assets/tiles/tile.png');
-    this.load.image('mario', 'assets/character/mario.png');
+    this.load.atlas('idle', 'assets/character/idle.png', 'assets/character/idle.json');
+    this.load.atlas('run', 'assets/character/run.png', 'assets/character/run2.json');
   }
 
   create() {
-    this.background = this.add.tileSprite(0, 0, GameScene.width, GameScene.height, 'nightSky').setOrigin(0, 0);
+    this.backgroundSky = this.add.tileSprite(0, 0, GameScene.width, GameScene.height, 'sky').setOrigin(0, 0);
+    this.backgroundSky.setScrollFactor(0);
 
-    // this.add.image(GameScene.width, GameScene.height, 'nightSky');
-    // this.background = this.add.tileSprite(GameScene.width/2, GameScene.height/2, GameScene.width, GameScene.height, 'nightSky');
-    // this.background.setDisplaySize(GameScene.width, GameScene.height);
+    // create character(player)
+    this.character = this.physics.add.sprite(10, GameScene.height/2, 'idle');
+    this.character.scale = 1.5;
+    this.character.setCollideWorldBounds(true);
 
-    this.character = this.physics.add.sprite(this.cameras.main.centerX, this.cameras.main.centerY, 'mario');
-    this.character.setDisplaySize(GameScene.width / 7, GameScene.width / 4);
-    this.character.setGravityY(400);
+    // set gravity of player
+    this.character.setGravityY(800);
 
-    for (let i = 0; i < 3; i++) {
-      const xPos = Math.floor(Math.random() * GameScene.width - 100);
-      const yPos = Math.floor(Math.random() * GameScene.height - 100);
+    this.anims.create({
+      key: 'standing',
+      frames: this.anims.generateFrameNames('idle', {prefix: 'idle', end: 19, zeroPad: 3 }),
+      frameRate: 5,
+      repeat: -1,
+    })
 
-      this.grounds[i] = this.physics.add.sprite(xPos <= 0 ? 1 : xPos, yPos <= 0 ? 1 : yPos , 'ground').setOrigin(0, 0);
-      this.grounds[i].setDisplaySize(100, 30);
-      this.grounds[i].setSize(100,30);
-      this.grounds[i].setImmovable();
+    this.anims.create({
+      key: 'running',
+      frames: this.anims.generateFrameNames('run', {prefix: 'sprite', end: 24, zeroPad: 3 }),
+      repeat: -1,
+    })
+    
+    this.character.anims.play('standing');
 
-    }
+    // create grounds
+    // for (let i = 0; i < 3; i++) {
+    //   const xPos = Math.floor(Math.random() * GameScene.width - 100);
+    //   const yPos = Math.floor(Math.random() * GameScene.height - 100);
+
+    //   this.grounds[i] = this.physics.add.sprite(xPos <= 0 ? 1 : xPos, yPos <= 0 ? 1 : yPos , 'ground').setOrigin(0, 0);
+    //   this.grounds[i].setDisplaySize(100, 30);
+    //   this.grounds[i].setSize(100,30);
+    //   this.grounds[i].setImmovable();
+    //   this.grounds[i].body.checkCollision.down = false;
+    //   this.grounds[i].body.checkCollision.left = false;
+    //   this.grounds[i].body.checkCollision.right = false;
+    // }
 
     this.grounds.push(this.physics.add.sprite(0, GameScene.height-10, 'ground').setOrigin(0,0).setDisplaySize(GameScene.width, 10).setImmovable());
     // ground.displayWidth = GameScene.width;
@@ -57,7 +79,15 @@ class GameScene extends Phaser.Scene {
     this.cursors = this.input.keyboard.createCursorKeys();
     this.pointer = this.input.activePointer;
 
-    this.background.setScrollFactor(0);
+    this.cameras.main.startFollow(this.character, true, 0.05, 0, -200, 0);
+    this.shakeCamera();
+
+    this.cameras.main.scaleManager.resize(GameScene.width*0.9, GameScene.height*0.9);
+    // this.cameras.main.setPosition(GameScene.width*0.1, GameScene.height*0.1);
+    this.outerCam = this.cameras.add(0,0, GameScene.width, GameScene.height, false, "outside");
+    this.outerCam.setBackgroundColor("#808080").setAlpha(0.5).scaleManager.resize(GameScene.width, GameScene.height);
+    
+
     // var particles = this.add.particles('red');
 
     // var emitter = particles.createEmitter({
@@ -76,40 +106,45 @@ class GameScene extends Phaser.Scene {
   }
 
   update() {
+    this.grounds[0].setDisplaySize(this.character.body.x + GameScene.width, 20);
+    this.physics.world.setBounds(0,0, this.character.body.position.x+GameScene.width, this.physics.world.bounds.height);
 
-    this.physics.world.setBounds(0,this.cameras.main.scrollY, this.physics.world.bounds.width, this.physics.world.bounds.height);
+    if(this.cameras.main.scrollX <= 0) {this.cameras.main.scrollX = 0;}
 
-    this.cameras.main.setLerp(.5);
-		this.cameras.main.centerOnY(this.character.y);
-
-    if(this.cameras.main.scrollY >= 0) {this.cameras.main.scrollY = 0;}
-
-    // console.log(this.cameras.main.scrollY);
-    this.background.tilePositionY = this.cameras.main.scrollY;
-
-    if (this.character.y > GameScene.height) {
-      this.character.y = GameScene.width / 2;
-    }
+    this.backgroundSky.tilePositionX = this.cameras.main.scrollX;
 
     if (this.cursors.left.isDown || (this.input.activePointer.isDown && this.input.activePointer.position.x < this.character.body.position.x)) {
-      this.character.setVelocityX(-160);
+      this.character.setVelocityX(-200);
 
-      this.character.anims.play('left', true);
+      if(!this.character.flipX){
+        this.character.setFlipX(true);
+      }
+      this.character.anims.play('running', true);
     }
     else if (this.cursors.right.isDown || (this.input.activePointer.isDown && this.input.activePointer.position.x > this.character.body.position.x)) {
-      this.character.setVelocityX(160);
+      this.character.setVelocityX(200);
 
-      this.character.anims.play('right', true);
+      if(this.character.flipX){
+        this.character.setFlipX(false);
+      }
+      this.character.anims.play('running', true);
     }
     else {
       this.character.setVelocityX(0);
 
-      this.character.anims.play('turn');
+      this.character.anims.play('standing', true);
     }
 
-    if (this.character.body.touching.down) {
-      this.character.setVelocityY(-930);
+    if (this.cursors.up.isDown && this.character.body.touching.down) {
+      this.character.setVelocityY(-500);
     }
+  }
+
+  shakeCamera(){
+    setTimeout(() => {
+      this.cameras.main.shake(1000, 0.003);
+      this.shakeCamera();
+    }, 2000)
   }
 }
 
@@ -125,8 +160,8 @@ export class GamePage implements OnInit {
   config: Phaser.Types.Core.GameConfig;
 
   constructor(platform: Platform) {
-    const width: number = platform.width() < 430 ? platform.width() : 430;
-    const height: number = platform.height() < 780 ? platform.height() : 780;
+    const width: number = platform.width() < 780 ? platform.width() : 780;
+    const height: number = platform.height() < 430 ? platform.height() : 430;
 
     this.config = {
       type: Phaser.AUTO,
@@ -140,8 +175,6 @@ export class GamePage implements OnInit {
     };
 
     GameScene.platform = platform;
-    // GameScene.width = platform.width();
-    // GameScene.height = platform.height();
     GameScene.width = width;
     GameScene.height = height;
   }
